ddc55752a3c8ad1913496c1ad6311511f4309a4a
Sebastian Sebald <sebastian.sebald@gmail.com>
2015-07-09 09:25:20 +0200

    Remove `__build` folder from git.

--- __build/main.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    // Toggle navigation menu
-    var toggle_element = document.querySelector('toggle-menu');
-    toggle_element.addEventListener('click', toggleMenu);
-    
-    function toggleMenu () {
-        console.log('clicked');
-    }
-})();

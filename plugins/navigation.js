58a22f17c7d64637981d4c7f564141be5b1c91a0
Sebastian Sebald <sebastian.sebald@gmail.com>
2015-07-10 15:41:41 +0200

    Add next/prev navigation to pages.

--- plugins/navigation.js
+++ plugins/navigation.js
@@ -6,7 +6,7 @@ var _ = require('lodash');
 function Navigation ( options ) {
     var defaults,
         config;
-        
+
     defaults = {
         name: 'nav',
         sort: compare
@@ -17,46 +17,55 @@ function Navigation ( options ) {
     return function ( files, metalsmith, done ) {
         var groups = {},
             dir;
-        
-        _.forEach(files, function ( file, path ) {
+
+        _.forEach(files, function ( file, path, idx ) {
             dir = path.match(/^(?:\.\/)?([^/]+)\/.*/);
-            
+
             // Ignore files on root directory
             if( dir ) {
                 dir = dir[1];
-                
+
                 if ( !groups[dir] ) {
                     groups[dir] = [];
                 }
-                
+
                 groups[dir].push({
                     title: file.title,
-                    path: path.replace(/index\.html$/, '')
+                    path: path.replace(/index\.html$/, ''),
+                    ref: path
                 });
             }
         });
-        
+
         groups = Object.keys(groups)
-            .map(function (key) { 
-                return { name: key, items: groups[key] }; 
+            .map(function (key) {
+                return { name: key, items: groups[key] };
             })
             .sort(config.sort);
+
+        // After everything is sorted, we can create next/prev links
+        _.forEach(groups, function ( grp ) {
+            _.forEach(grp.items, function ( item, idx ) {
+                var file = files[item.ref];
+                file.prev = (idx > 0) ? grp.items[idx - 1].ref : null;
+                file.next = (idx < grp.items.length-1) ? grp.items[idx + 1].ref : null;
+            });
+        });
 
         metalsmith._metadata[config.name] = groups;
         done();
     }
-    
-    
-    
+
+
     function compare ( a, b ) {
         var na = a.name.toLowerCase(),
             nb = b.name.toLowerCase();
-            
+
         // Sort by order if specified
         if( _.isArray(config.order) ) {
             return config.order.indexOf(na) - config.order.indexOf(nb);
         }
-        
+
         // Fallback sort by group name
         return na.localeCompare(nb);
     }

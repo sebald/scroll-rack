6d851ea1977103aa03f0d5ce8b8993323d815de0
Sebastian Sebald <sebastian.sebald@gmail.com>
2015-07-10 13:05:11 +0200

    Make Sass plugin more robust
    -> will try to use previous build if current build fails.

--- plugins/sass.js
+++ plugins/sass.js
@@ -1,6 +1,8 @@
 /// <reference path="../typings/tsd.d.ts"/>
 
 var _ = require('lodash'),
+    chalk = require('chalk'),
+    log = require('./../helpers/log'),
     path = require('path'),
     sass = require('node-sass');
 
@@ -10,33 +12,50 @@ function Sass ( config ) {
         var file = path.basename(filePath);
         return file.replace(/.s[ca]ss$/, '.css');
     }
-    
+
     // Conform metalsmith API
     return function ( files, metalsmith, done ) {
         var fileName;
-        
+
         // Normalize settings
         config.file = metalsmith.path(config.file);
         config.outputDir = config.outDir || './' ;
         config.outFile = path.join(
-            config.outputDir, 
+            config.outputDir,
             parseFileName(config.file)
         );
         fileName = config.outFile; // node-sass messes with the config >.<
 
         sass.render( config, function ( err, result ) {
-            if ( err ) { throw err; }
+            if ( err ) {
+                log(
+                    chalk.red('ERROR: Sass could not be compiled!\n\n') +
+                    chalk.red(err)
+                );
+
+                // Use previous sass build if possible.
+                if( metalsmith._metadata['sass_backup'] ) {
+                    log(chalk.white('Using last sass build.'))
+                    result = metalsmith._metadata['sass_backup'];
+                }
+            }
 
             files[fileName] = {
                 contents: result.css
             };
-            
+
             if( result.map ) {
                 files[fileName + '.map'] = {
                     contents: result.map
                 };
             }
-            
+
+            // Write a backup fale in case next time rendering fails.
+            metalsmith._metadata['sass_backup'] = {
+                css: result.css,
+                map: result.map
+            };
+
             done();
         });
     };
